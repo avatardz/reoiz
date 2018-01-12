7fe01bfb53c1a6dc450e36859f7675d334647824
avatardz <35350995+avatardz@users.noreply.github.com>
2018-01-11 17:45:58 -0800

    Add files via upload

---
+++ wearAvatar.js
@@ -0,0 +1,13 @@
+(function(){ 
+    this.replaceAvatar = function(entityID) {
+        MyAvatar.useFullAvatarURL(Entities.getEntityProperties(entityID,"modelURL").modelURL);
+    };
+    this.replaceAvatarByMouse = function(entityID, mouseEvent) {
+        if (mouseEvent.isLeftButton) {
+            this.replaceAvatar(entityID);
+        }
+    };
+    this.clickDownOnEntity = this.replaceAvatarByMouse; 
+    this.startFarTrigger = this.replaceAvatar; 
+    this.startNearTrigger = this.replaceAvatar; 
+})
